1414c23f3e567d5fd368fdfd3b1353f322f9b22f
j-m-t <trevino.jm@protonmail.com>
2018-08-07 18:57:46 -0400

    Added config for thermometer

--- weather_conky_draw.lua
+++ weather_conky_draw.lua
@@ -1,5 +1,19 @@
 elements = {
   {
+    -- Thermometer for current temperature
+    kind = 'thermometer',
+    temp = 'execpi 90 sed -n "3p" /path/to/weather/conditions',
+    maxtemp = 40,
+    center = {x = 120, y = 105},
+    radius = 5,
+    color = 0x00E5FF,
+    alpha = 1,
+    thickness = 1.5,
+    height = 25,
+    colors = {0x00FFFF, 0xFFFFFF, 0xFFA500, 0xFF0000},
+    grades = {10, 20, 30}
+  },
+  {
     -- Color variable text for current temperature
     kind = 'temperature_text',
     scale = 'execpi 90 sed -n "40p" /path/to/weather/conditions',
